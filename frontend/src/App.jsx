e6b788d9e4b7badcc2d96db53cc57dcd3b5484cf
Imraj <imrajrabbani2003@gmail.com>
2025-04-19 10:15:09 +0600

    Initial Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer/>
+      <Routes>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
