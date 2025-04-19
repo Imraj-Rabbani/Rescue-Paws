dfdbe8a54f3d8cd6761697214953a7ba2ec97f5e
Imraj <imrajrabbani2003@gmail.com>
2025-04-19 10:45:05 +0600

    feat: add the API endpoints in server file

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import "dotenv/config"
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
-
+import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use(cors({origin: allowedOrigins ,credentials: true}))
 
 //API Endpoints
 app.get('/', (req, res )=> res.send("API Working"))
-
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`))
