4b676cf4cde1df2417df73b0ff6b6169c1f17f0b
Imraj <imrajrabbani2003@gmail.com>
2025-04-19 10:54:23 +0600

    fix: fixing typos that caused an error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
+import Login from './pages/Login'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -9,7 +10,6 @@ const App = () => {
       <ToastContainer/>
       <Routes>
         <Route path='/login'element={<Login/>} />
-        <Route path='/email-verify'element={<EmailVerify/>} />
       </Routes>
     </div>
   )
