fb17401804e95715e3443e8e23827992df864fcf
Imraj <imrajrabbani2003@gmail.com>
2025-04-19 10:47:50 +0600

    feat: add the context file required for auth

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,1 +1,24 @@
-//Context file
+import { useState } from "react";
+import { createContext } from "react";
+
+export const AppContent = createContext()
+
+export const AppContextProvider = (props) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [userData, setUserData] = useState(false)
+
+
+    const value = {
+        backendUrl,
+        isLoggedIn, setIsLoggedIn,
+        userData, setUserData
+    }
+
+    return(
+        <AppContent.Provider value={value}>
+            {props.children}
+        </AppContent.Provider>
+    )
+}
