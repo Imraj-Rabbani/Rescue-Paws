236b5f8f20bce766b8037f7d1db27e2a673c8c23
Imraj <imrajrabbani2003@gmail.com>
2025-04-24 20:49:37 +0600

    feat: added functionality to check auth status

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -4,7 +4,7 @@ const userAuth = async (req, res, next) => {
     const {token} = req.cookies;
 
     if(!token){
-        return res.json({success: false, message: "Not Authorized. Login Again"})      
+        return res.json({success: false, message: "Not Authorized. Login Again middleware e asi", cookies: req.cookies})      
     }
 
     try {
@@ -14,7 +14,7 @@ const userAuth = async (req, res, next) => {
         if(tokenDecode.id){
             req.userId = tokenDecode.id
         }else{
-            return res.json({success: false, message: "Not Authorized. Login agains"})
+            return res.json({success: false, message: "Not Authorized. Login agains new line e ashchi"})
         }
         next()
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,109 @@
-import { Link } from 'react-router-dom'
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
 export default function Navbar() {
+  const { isLoggedIn, setIsLoggedIn, userData, backendUrl } =
+    useContext(AppContext);
+  // console.log(userData);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        `${backendUrl}/api/auth/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      setIsLoggedIn(false);
+      toast.success("Logged out successfully");
+      navigate("/login");
+    } catch (error) {
+      toast.error("Logout failed");
+      console.error("Logout error:", error);
+    }
+  };
+
   return (
-    <nav className="flex justify-between items-center p-6 bg-white shadow-md">
-      <div className="text-xl font-bold">STRAY PAWS</div>
+    <nav className="flex justify-between items-center bg-white shadow-md p-2">
+      <div 
+        className="cursor-pointer" 
+        onClick={() => navigate("/")}
+      >
+      <img src="/logo_new.png" alt="STRAY PAWS" className=" ml-4 w-[100px]" />
+      </div>
+
       <ul className="flex gap-6">
-        <li><a href="#" className="hover:underline">Home</a></li>
-        <li><a href="#" className="hover:underline">Teams</a></li>
-        <li><a href="#" className="hover:underline">Rescues</a></li>
-        <li><Link to="/products" className="hover:underline">Products</Link></li>
-        <li><a href="#" className="hover:underline">Analytics</a></li>
+        <li>
+          <a href="#" className="hover:underline" onClick={() => navigate("/")}>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline" onClick={() => navigate("/")}>
+            Teams
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline" onClick={() => navigate("/")}>
+            Rescues
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="hover:underline"
+            onClick={() => navigate("/products")}
+          >
+            Products
+          </a>
+        </li>
+        {isLoggedIn && (
+          <li>
+            <a
+              href="#"
+              className="hover:underline"
+              onClick={() => navigate("/")}
+            >
+              Analytics
+            </a>
+          </li>
+        )}
       </ul>
-      <div>
-        <a href="http://localhost:5173/login" className="text-sm font-semibold hover:underline">Register</a>
+
+      <div className="flex items-center gap-4">
+        {isLoggedIn ? (
+          <>
+            <div className="flex items-center gap-8">
+              <span className="text-sm font-semibold">{userData.name}</span>
+              <button
+                onClick={handleLogout}
+                className="text-sm font-semibold hover:underline"
+              >
+                Logout
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={() => navigate("/login")}
+              className="text-sm font-semibold hover:underline"
+            >
+              Login
+            </button>
+            <button
+              onClick={() => navigate("/login")}
+              className="text-sm font-semibold hover:underline"
+            >
+              Register
+            </button>
+          </>
+        )}
       </div>
     </nav>
   );
